5b80208915282434a50cd8d23fceca1928e30b30
artem-kondratew <artemkondratev5@gmail.com>
2023-12-29 14:19:21 +0300

    sketch for encoder testing on arduino leonardo

---
+++ workspace/src/arduino/test_encoder/test_encoder.ino
@@ -0,0 +1,38 @@
+#define ENCA 2
+#define ENCB 3
+
+
+int64_t pose = 0;
+
+
+int64_t readEncoder() {
+    int b = digitalRead(ENCB);
+    if (b > 0) {
+        pose++;
+    }
+    else {
+        pose--;
+    }
+}
+
+
+void setup() {
+    pinMode(ENCA, INPUT);
+    pinMode(ENCB, INPUT);
+
+    attachInterrupt(digitalPinToInterrupt(ENCA), readEncoder, RISING);
+    
+    Serial.begin(2000000);
+    Serial.setTimeout(0);
+}
+
+void loop() {
+
+    Serial.println((int)pose);
+/*
+    Serial.print(digitalRead(ENCA));
+    Serial.print(" ");
+    Serial.println(digitalRead(ENCB));
+    delay(100);
+*/
+}
