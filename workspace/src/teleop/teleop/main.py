16b2b7ea6a0f54b8e96b95b3b6a0d5843703801d
artem-kondratew <artemkondratev5@gmail.com>
2024-06-20 18:57:42 +0300

    add custom teleop with odom reset feature

---
+++ workspace/src/teleop/teleop/main.py
@@ -0,0 +1,212 @@
+import sys
+import threading
+
+import geometry_msgs.msg
+import rclpy
+from std_msgs.msg import Bool
+
+import termios
+import tty
+
+
+msg = """
+This node takes keypresses from the keyboard and publishes them
+as Twist/TwistStamped messages. It works best with a US keyboard layout.
+---------------------------
+Moving around:
+   u    i    o
+   j    k    l
+   m    ,    .
+
+For Holonomic mode (strafing), hold down the shift key:
+---------------------------
+   U    I    O
+   J    K    L
+   M    <    >
+
+t : up (+z)
+b : down (-z)
+
+anything else : stop
+
+q/z : increase/decrease max speeds by 10%
+w/x : increase/decrease only linear speed by 10%
+e/c : increase/decrease only angular speed by 10%
+
+CTRL-C to quit
+"""
+
+moveBindings = {
+    'i': (1, 0, 0, 0),
+    'o': (1, 0, 0, -1),
+    'j': (0, 0, 0, 1),
+    'l': (0, 0, 0, -1),
+    'u': (1, 0, 0, 1),
+    ',': (-1, 0, 0, 0),
+    '.': (-1, 0, 0, 1),
+    'm': (-1, 0, 0, -1),
+    'O': (1, -1, 0, 0),
+    'I': (1, 0, 0, 0),
+    'J': (0, 1, 0, 0),
+    'L': (0, -1, 0, 0),
+    'U': (1, 1, 0, 0),
+    '<': (-1, 0, 0, 0),
+    '>': (-1, -1, 0, 0),
+    'M': (-1, 1, 0, 0),
+    't': (0, 0, 1, 0),
+    'b': (0, 0, -1, 0),
+}
+
+speedBindings = {
+    'q': (0.01, 0.01),
+    'z': (-0.01, -0.01),
+    'w': (0.01, 0.00),
+    'x': (-0.01, 0.00),
+    'e': (0.00, 0.01),
+    'c': (0.00, -0.01),
+}
+
+resetBinding = {
+    'r': 0,
+}
+
+
+def getKey(settings):
+    tty.setraw(sys.stdin.fileno())
+    # sys.stdin.read() returns a string on Linux
+    key = sys.stdin.read(1)
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+    return key
+
+
+def saveTerminalSettings():
+    return termios.tcgetattr(sys.stdin)
+
+
+def restoreTerminalSettings(old_settings):
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
+
+
+def vels(speed, turn):
+    return 'currently:\tspeed %s\tturn %s ' % (round(speed, 2), round(turn, 2))
+
+
+def main():
+    settings = saveTerminalSettings()
+
+    rclpy.init()
+
+    node = rclpy.create_node('robot_teleop')
+
+    # parameters
+    stamped = node.declare_parameter('stamped', False).value
+    frame_id = node.declare_parameter('frame_id', '').value
+
+    is_reset_cmd_last = False
+
+    if not stamped and frame_id:
+        raise Exception("'frame_id' can only be set when 'stamped' is True")
+
+    if stamped:
+        TwistMsg = geometry_msgs.msg.TwistStamped
+    else:
+        TwistMsg = geometry_msgs.msg.Twist
+
+    pub = node.create_publisher(TwistMsg, 'cmd_vel', 10)
+    reset_pub = node.create_publisher(Bool, '/drive_controller/reset', 10)
+
+    spinner = threading.Thread(target=rclpy.spin, args=(node,))
+    spinner.start()
+
+    speed = 0.2
+    turn = 0.5
+    x = 0.0
+    y = 0.0
+    z = 0.0
+    th = 0.0
+    status = 0.0
+
+    twist_msg = TwistMsg()
+
+    if stamped:
+        twist = twist_msg.twist
+        twist_msg.header.stamp = node.get_clock().now().to_msg()
+        twist_msg.header.frame_id = frame_id
+    else:
+        twist = twist_msg
+
+    try:
+        print(msg)
+        print(vels(speed, turn))
+        while True:
+            key = getKey(settings)
+            if key in moveBindings.keys():
+                x = moveBindings[key][0]
+                y = moveBindings[key][1]
+                z = moveBindings[key][2]
+                th = moveBindings[key][3]
+                if is_reset_cmd_last:
+                    print(vels(speed, turn))
+                is_reset_cmd_last = False
+            
+            elif key in speedBindings.keys():
+                speed = speed + speedBindings[key][0]
+                turn = turn + speedBindings[key][1]
+                print(vels(speed, turn))
+                if (status == 14):
+                    print(msg)
+                status = (status + 1) % 15
+                is_reset_cmd_last = False
+
+            elif key in resetBinding.keys():
+                reset_msg = Bool()
+                reset_msg.data = True
+                reset_pub.publish(reset_msg)
+                print('reset sent')
+                if (status == 14):
+                    print(msg)
+                status = (status + 1) % 15
+                is_reset_cmd_last = True
+            
+            else:
+                is_reset_cmd_last = False
+                x = 0.0
+                y = 0.0
+                z = 0.0
+                th = 0.0
+                if (key == '\x03'):
+                    break
+
+            if stamped:
+                twist_msg.header.stamp = node.get_clock().now().to_msg()
+
+            twist.linear.x = x * speed
+            twist.linear.y = y * speed
+            twist.linear.z = z * speed
+            twist.angular.x = 0.0
+            twist.angular.y = 0.0
+            twist.angular.z = th * turn
+            pub.publish(twist_msg)
+
+    except Exception as e:
+        print(e)
+
+    finally:
+        if stamped:
+            twist_msg.header.stamp = node.get_clock().now().to_msg()
+
+        twist.linear.x = 0.0
+        twist.linear.y = 0.0
+        twist.linear.z = 0.0
+        twist.angular.x = 0.0
+        twist.angular.y = 0.0
+        twist.angular.z = 0.0
+        pub.publish(twist_msg)
+        rclpy.shutdown()
+        spinner.join()
+
+        restoreTerminalSettings(settings)
+
+
+if __name__ == '__main__':
+    main()
