fd1b3c2beaf0d80c8b1d67648a80337fab587f97
artem-kondratew <artemkondratev5@gmail.com>
2024-06-20 19:00:01 +0300

    working version with serial

--- workspace/src/drive_controller/launch/drive_controller.launch.py
+++ workspace/src/drive_controller/launch/drive_controller.launch.py
@@ -14,7 +14,7 @@ rviz_file = os.path.join(get_package_share_directory(package_name), 'config', 'o
 ros_serial = Node(
     package=package_name,
     executable='ros_serial',
-    name='ros_serial',
+    name='drive_controller_serial',
     parameters=[params_file],
 )
 
@@ -25,28 +25,11 @@ drive_controller = Node(
     parameters=[params_file],
 )
 
-keyboard_handler = Node(
-    package=package_name,
-    executable='keyboard_handler',
-    name='keyboard_handler',
-    parameters=[params_file],
-)
-
-rviz = Node(
-    package='rviz2',
-    namespace='',
-    executable='rviz2',
-    name='rviz2',
-    arguments=['-d', rviz_file]
-)
-
 
 def generate_launch_description():
     ld = LaunchDescription()
 
-    # ld.add_action(ros_serial)
+    ld.add_action(ros_serial)
     ld.add_action(drive_controller)
-    # ld.add_action(keyboard_handler)
-    # ld.add_action(rviz)
     
     return ld
